74c457619c50d29ddd2945650364023f6b8c4222
lucadenuca <luka.filipovic.fon@gmail.com>
2022-05-28 03:16:00 +0200

    V -> Api resource rute i resource kontroleri za gradilište i šefa

---
+++ app/Http/Controllers/GradilisteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gradiliste;
+use Illuminate\Http\Request;
+
+class GradilisteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Gradiliste  $gradiliste
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gradiliste $gradiliste)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Gradiliste  $gradiliste
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Gradiliste $gradiliste)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Gradiliste  $gradiliste
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Gradiliste $gradiliste)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Gradiliste  $gradiliste
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gradiliste $gradiliste)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\GradilisteController;
+use App\Http\Controllers\SefController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::resource('gradiliste', GradilisteController::class);
+Route::resource('sef', SefController::class);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
